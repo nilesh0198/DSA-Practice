d5f7a83dda3cef90825c1ad6fe7853bf48333050
Nilesh Kumar <145326646+nilesh0198@users.noreply.github.com>
2025-04-17 21:45:20 +0530

    Time: 0 ms (100%), Space: 15.4 MB (64.71%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         int count = 0;
         int n = nums.size();
 
-        for (int i = 0; i < n ; i++) {
+        for (int i = 0; i < n-1; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (nums[i] == nums[j] && ((long long)i * j) % k == 0) {
                     count++;
